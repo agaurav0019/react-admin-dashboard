b76894edadbca5feddcc91492a2c0e0f4fe8e099
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2023-05-08 02:43:01 +0530

    Added sidebar and Topbar.

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -1,9 +1,13 @@
-const Dashboard = () =>{
-    return (
-        <>
-        <div>Dashboard</div>
-        </>
-    )
-}
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box dislplay="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to Dashboard" /> 
+      </Box>
+    </Box>
+  );
+};
 
 export default Dashboard;
